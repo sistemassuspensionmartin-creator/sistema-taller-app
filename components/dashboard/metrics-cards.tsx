75b025cb77f9700f069228091358ae1f02987f96
sistemassuspensionmartin-creator <sistemas.suspensionmartin@gmail.com>
2026-04-03 18:23:57 -0300

    aver

--- components/dashboard/metrics-cards.tsx
+++ components/dashboard/metrics-cards.tsx
@@ -10,7 +10,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Badge } from "@/components/ui/badge"
 
-export function OverviewView({ 
+export function MetricsCards({  
   onNavigateToPresupuestos, 
   onNavigateToTurnos, 
   onNavigateToCaja 
